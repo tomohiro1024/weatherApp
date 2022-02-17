8babdc51eb5d001c7139ccd14a011363a38c73dd
tomohiro1024 <gongyezhihong732@gmail.com>
2022-02-17 22:45:34 +0900

    エラーメッセージの追加

--- lib/top_page.dart
+++ lib/top_page.dart
@@ -14,6 +14,11 @@ class _TopPageState extends State<TopPage> {
   // 現在の天気情報
   Weather currentWeather = Weather(
       10, 15, 5, '晴れ', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30, 12, 10), 50, 10);
+
+  String? address = 'ー';
+
+  String? errorMessage;
+
   // 1時間ごとの天気情報
   List<Weather> hourlyWeather = [
     Weather(10, 15, 5, '晴れ', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30, 10), 50, 10),
@@ -29,138 +34,144 @@ class _TopPageState extends State<TopPage> {
     Weather(12, 15, 5, '雨', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30, 12), 70, 10),
     Weather(13, 15, 5, '台風', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30, 13), 80, 10),
   ];
-  // 日毎の天気情報
-  List<Weather> dailyWeather = [
-    Weather(10, 15, 5, '晴れ', 5.5, 5.5, '晴れ', DateTime(2022, 1, 27), 50, 10),
-    Weather(11, 16, 6, '曇り', 5.5, 5.5, '晴れ', DateTime(2022, 1, 28), 60, 10),
-    Weather(12, 17, 7, '雨', 5.5, 5.5, '晴れ', DateTime(2022, 1, 29), 70, 10),
-    Weather(13, 18, 8, '台風', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30), 80, 10),
-    Weather(10, 15, 5, '晴れ', 5.5, 5.5, '晴れ', DateTime(2022, 1, 27), 50, 10),
-    Weather(11, 16, 6, '曇り', 5.5, 5.5, '晴れ', DateTime(2022, 1, 28), 60, 10),
-    Weather(12, 17, 7, '雨', 5.5, 5.5, '晴れ', DateTime(2022, 1, 29), 70, 10),
-    Weather(13, 18, 8, '台風', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30), 80, 10),
-    Weather(10, 15, 5, '晴れ', 5.5, 5.5, '晴れ', DateTime(2022, 1, 27), 50, 10),
-    Weather(11, 16, 6, '曇り', 5.5, 5.5, '晴れ', DateTime(2022, 1, 28), 60, 10),
-    Weather(12, 17, 7, '雨', 5.5, 5.5, '晴れ', DateTime(2022, 1, 29), 70, 10),
-    Weather(13, 18, 8, '台風', 5.5, 5.5, '晴れ', DateTime(2022, 1, 30), 80, 10),
-  ];
-
-  List<String> weekDay = ['月', '火', '水', '木', '金', '土', '日'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Container(
-                    width: 220,
-                    child: TextField(
-                      onSubmitted: (value) async {
-                        String? address;
-                        address = await ZipCode.searchAddressFromZipCode(value);
-                        print(address);
-                      },
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(hintText: '郵便番号を入力して下さい'),
-                    )),
-                SizedBox(height: 20),
-                Text(
-                  'さいたま市',
-                  style: TextStyle(fontSize: 50),
-                ),
-                Text(
-                  '${currentWeather.temp}°',
-                  style: TextStyle(fontSize: 50),
-                ),
-                Text(currentWeather.description),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 10.0),
-                      child: Text('最高気温: ${currentWeather.tempMax}°'),
-                    ),
-                    Text('最低気温: ${currentWeather.tempMin}°'),
-                  ],
-                ),
-                SizedBox(height: 20),
-                // 時間毎のUI
-                Divider(height: 0),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: hourlyWeather.map((weather) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 9, vertical: 8),
-                        child: Column(
-                          children: [
-                            Text('${DateFormat('H').format(weather.time)}時'),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: Icon(Icons.light_mode),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: Text(
-                                '${weather.temp}°',
-                                style: TextStyle(fontSize: 19),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }).toList(),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Container(
+                      width: 220,
+                      child: TextField(
+                        onSubmitted: (value) async {
+                          Map<String, String> response = {};
+                          response =
+                              (await ZipCode.searchAddressFromZipCode(value))!;
+                          // responseの'message'というキーに値が入ってきた(エラーが出た)場合に入ってくる変数
+                          errorMessage = response['message'];
+                          if (errorMessage != null) {
+                            final snackBar = SnackBar(
+                              backgroundColor: Colors.red,
+                              content: Text(errorMessage!),
+                            );
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          }
+                          // responseの'address'というキーが含んでいた場合、変数addressに'address'の値を入れる
+                          if (response.containsKey('address')) {
+                            address = response['address'];
+                          }
+                          print(response);
+                          setState(() {});
+                        },
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(hintText: '郵便番号を入力して下さい'),
+                      )),
+                  // errorMessageに値が入っていない(エラーじゃない)場合何も表示しない
+                  // Text(
+                  //   errorMessage == null ? '' : errorMessage!,
+                  //   style: TextStyle(color: Colors.red),
+                  // ),
+                  SizedBox(height: 30),
+                  Text(
+                    address!,
+                    style: TextStyle(fontSize: 30),
                   ),
-                ),
-                Divider(height: 0),
-                // 日毎のUI
-                // Expanded(
-                //   child: SingleChildScrollView(
-                //     child: Padding(
-                //       padding: const EdgeInsets.symmetric(horizontal: 9.0),
-                //       child: Column(
-                //         children: dailyWeather.map((weather) {
-                //           return Container(
-                //             height: 50,
-                //             child: Row(
-                //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //               children: [
-                //                 Container(
-                //                     width: 50,
-                //                     child: Text(
-                //                         '${weekDay[weather.time.weekday - 1]}曜日')),
-                //                 Icon(Icons.light_mode),
-                //                 Container(
-                //                   width: 50,
-                //                   child: Row(
-                //                     mainAxisAlignment:
-                //                         MainAxisAlignment.spaceBetween,
-                //                     children: [
-                //                       Text('${weather.tempMax}°',
-                //                           style: TextStyle(fontSize: 17)),
-                //                       Text('${weather.tempMin}°',
-                //                           style: TextStyle(
-                //                               fontSize: 17,
-                //                               color:
-                //                                   Colors.black.withOpacity(0.5))),
-                //                     ],
-                //                   ),
-                //                 ),
-                //               ],
-                //             ),
-                //           );
-                //         }).toList(),
-                //       ),
-                //     ),
-                //   ),
-                // )
-              ],
-            ),
-          ],
+                  Text(
+                    '${currentWeather.temp}°',
+                    style: TextStyle(fontSize: 50),
+                  ),
+                  Text(currentWeather.description),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        child: Text('最高気温: ${currentWeather.tempMax}°'),
+                      ),
+                      Text('最低気温: ${currentWeather.tempMin}°'),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // 時間毎のUI
+                  Divider(height: 0),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: hourlyWeather.map((weather) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 9, vertical: 8),
+                          child: Column(
+                            children: [
+                              Text('${DateFormat('H').format(weather.time)}時'),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: Icon(Icons.light_mode),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: Text(
+                                  '${weather.temp}°',
+                                  style: TextStyle(fontSize: 19),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Divider(height: 0),
+                  // 日毎のUI
+                  // Expanded(
+                  //   child: SingleChildScrollView(
+                  //     child: Padding(
+                  //       padding: const EdgeInsets.symmetric(horizontal: 9.0),
+                  //       child: Column(
+                  //         children: dailyWeather.map((weather) {
+                  //           return Container(
+                  //             height: 50,
+                  //             child: Row(
+                  //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //               children: [
+                  //                 Container(
+                  //                     width: 50,
+                  //                     child: Text(
+                  //                         '${weekDay[weather.time.weekday - 1]}曜日')),
+                  //                 Icon(Icons.light_mode),
+                  //                 Container(
+                  //                   width: 50,
+                  //                   child: Row(
+                  //                     mainAxisAlignment:
+                  //                         MainAxisAlignment.spaceBetween,
+                  //                     children: [
+                  //                       Text('${weather.tempMax}°',
+                  //                           style: TextStyle(fontSize: 17)),
+                  //                       Text('${weather.tempMin}°',
+                  //                           style: TextStyle(
+                  //                               fontSize: 17,
+                  //                               color:
+                  //                                   Colors.black.withOpacity(0.5))),
+                  //                     ],
+                  //                   ),
+                  //                 ),
+                  //               ],
+                  //             ),
+                  //           );
+                  //         }).toList(),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/zip_code.dart
+++ lib/zip_code.dart
@@ -3,7 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class ZipCode {
-  static Future<String?> searchAddressFromZipCode(String zipCode) async {
+  static Future<Map<String, String>?> searchAddressFromZipCode(
+      String zipCode) async {
     String url = 'https://zipcloud.ibsnet.co.jp/api/search?zipcode=$zipCode';
 
     try {
@@ -11,8 +12,20 @@ class ZipCode {
       var result = await get(Uri.parse(url));
       // jsonDecode()によってresultの中身がjson型からMap型に変換する。
       Map<String, dynamic> date = jsonDecode(result.body);
-      String address = date['results'][0]['address2'];
-      return address;
+      Map<String, String> response = {};
+      // jsonを取得してエラーが出た場合(7桁以外の数字が入力された場合)
+      if (date['message'] != null) {
+        response['message'] = '7桁の郵便番号を入力して下さい';
+      } else {
+        // 存在しない郵便番号等を入力した場合
+        if (date['results'] == null) {
+          response['message'] = '正しい郵便番号を入力して下さい';
+        } else {
+          // 正しい郵便番号が入力された場合
+          response['address'] = date['results'][0]['address2'];
+        }
+      }
+      return response;
     } catch (e) {
       print(e);
       return null;
