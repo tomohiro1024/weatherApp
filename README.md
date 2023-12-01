081beebbc5bb303a163df09ec06bd84aaa26fc8f
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-01 09:37:39 +0900

    add

--- README.md
+++ README.md
@@ -6,9 +6,8 @@
 ・郵便番号検索時のメッセージを増やす
 ・↑のメッセージをテキスト表示させる。
 ・初期表示時は、郵便番号テキストフィールド以外は、何も表示しない
-
 ・郵便番号を8桁以上入力できないようにする
-・送信ボタン連打防止する
+・FutureBuilderなどを使い、データが取得できるまでは、ローディングを表示させたい。
 
 
 
@@ -16,14 +15,13 @@
 ・現在の居場所を取得させる。(初期表示時)
 ・郵便番号の検索履歴を表示する(DB)
 
-keytool -genkey -v -keystore ./key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias release -storetype JKS
-
-keytool -genkey -v -keystore ./key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias key
-
-
-keytool -genkey -v -keystore ./key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias key
-
 keytool -genkey -v -keystore ./key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias key -storetype JKS
 
-
 open build/app/outputs/bundle
+
+
+
+
+
+
+
--- lib/top_page.dart
+++ lib/top_page.dart
@@ -70,7 +70,7 @@ class _TopPageState extends State<TopPage> {
                             setState(() {
                               isButtonEnabled = true;
                             });
-                            await Future.delayed(const Duration(seconds: 5),
+                            await Future.delayed(const Duration(seconds: 2),
                                 () {
                               setState(() {
                                 isButtonEnabled = false;
@@ -79,6 +79,10 @@ class _TopPageState extends State<TopPage> {
                             Map<String, String> response = {};
                             response = (await ZipCode.searchAddressFromZipCode(
                                 _textEditingController.text))!;
+
+                            print('responseresponse');
+
+                            print(response);
 
                             errorMessage = response['message'];
                             if (errorMessage != null) {
